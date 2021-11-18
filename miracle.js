7c0c2a1bfba1848fbb411c0613c527f5d6a08e19
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-11-18 20:44:50 +0100

    adding image

--- miracle.js
+++ miracle.js
@@ -2,23 +2,30 @@
 const btn = document.getElementById('btn');
 const output = document.getElementById('output');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', function getTheDate(){
           
       function miracleDate(start, end) {
         return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
       }
 
-      const miracle = miracleDate(new Date(2021, 0, 1), new Date(2021, 11, 31));
+      const miracle = miracleDate(new Date(2022, 0, 1), new Date(2022, 11, 31));
       console.log(miracle.getMonth());
 
+      if(miracle.getMonth() === 0){
+        return getTheDate();
+      }
 
-      output.innerHTML = `<h1>${miracle.getDate()} ${miracle.getMonth()} ${miracle.getFullYear()}</h1> `;
+
+      output.innerHTML = `<h1>${miracle.getDate()}.${miracle.getMonth()}.${miracle.getFullYear()}</h1> `;
 });
-
-
+ 
 /* 
- function miracleDate(start, end) {
-    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-  }
+var now = new Date();
+var start = new Date(now.getFullYear(), 0, 0);
+var diff = now - start;
+var oneDay = 1000 * 60 * 60 * 24;
+var day = Math.floor(diff / oneDay);
+console.log('Day of year: ' + day);
 
-  miracleDate(new Date(2021, 0, 1), new Date(2021, 11, 31)); */
+console.log(diff);
+ */
