378405998af6c12f2f6948ad828736313254bdc1
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-11-18 23:34:29 +0100

    comments added

--- miracle.js
+++ miracle.js
@@ -2,6 +2,8 @@
 const btn = document.getElementById('btn');
 const output = document.getElementById('output');
 
+//  The event listener starts a function which gives us a random date from 1.1. to 31.12.
+
 btn.addEventListener('click', function getTheDate(){
           
       function miracleDate(start, end) {
@@ -9,23 +11,13 @@ btn.addEventListener('click', function getTheDate(){
       }
 
       const miracle = miracleDate(new Date(2022, 0, 1), new Date(2022, 11, 31));
-      console.log(miracle.getMonth());
+      
 
       if(miracle.getMonth() === 0){
         return getTheDate();
       }
 
+      //  the above mentioned value will be displayed on the output div
 
       output.innerHTML = `<h1>${miracle.getDate()}.${miracle.getMonth()}.${miracle.getFullYear()}</h1> `;
 });
- 
-/* 
-var now = new Date();
-var start = new Date(now.getFullYear(), 0, 0);
-var diff = now - start;
-var oneDay = 1000 * 60 * 60 * 24;
-var day = Math.floor(diff / oneDay);
-console.log('Day of year: ' + day);
-
-console.log(diff);
- */
